1ca1f528e252fc4b03548d42a54f5b8ab6df248f
rpkrupali1 <rpkrupali@gmail.com>
2022-05-31 17:48:19 -0500

    updates to remaining pages to refactor based on new framework structure

--- src/test/java/core/framework/pages/ProductDetailPage.java
+++ src/test/java/core/framework/pages/ProductDetailPage.java
@@ -1,6 +1,7 @@
 package core.framework.pages;
 
 import core.framework.base.BasePage;
+import core.framework.controls.elements.Element;
 import core.framework.controls.elements.iElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
@@ -12,12 +13,20 @@ public class ProductDetailPage extends BasePage {
     @FindBy(how = How.XPATH, using = "//h1[normalize-space()='About this item']/following-sibling::ul")
     private iElement eleProductDescription;
 
+    public static Element eleAboutThisHeader() {
+        return new Element(getElement("xpath","//h1[normalize-space()='About this item']"),"About this header","product detail page");
+    }
+
+    public static Element eleProductDescription() {
+        return new Element(getElement("xpath","//h1[normalize-space()='About this item']/following-sibling::ul"),"Product description","product detail page");
+    }
+
     public boolean isAboutThisHeaderDisplayed(){
-        return eleAboutThisHeader.isDisplayed();
+        return eleAboutThisHeader().isDisplayed();
     }
 
     public String getProductDetails(){
-        return eleProductDescription.getText();
+        return eleProductDescription().getText();
     }
 
 }
--- src/test/java/core/framework/pages/ProductListingPage.java
+++ src/test/java/core/framework/pages/ProductListingPage.java
@@ -1,29 +1,41 @@
 package core.framework.pages;
 
 import core.framework.base.BasePage;
+import core.framework.controls.elements.Element;
 import core.framework.controls.elements.iElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
 public class ProductListingPage extends BasePage {
-    @FindBy(how = How.XPATH, using = "//span[*[text()='Sort by:']]")
-    private iElement eleSortBy;
+//    @FindBy(how = How.XPATH, using = "//span[*[text()='Sort by:']]")
+//    private iElement eleSortBy;
+//
+//    @FindBy(how = How.XPATH, using = "//*[@class='a-popover-inner']/ul/li/a[text()='Price: High to Low']")
+//    private iElement elePriceHighToLow;
+//
+//    @FindBy(how = How.XPATH, using = "//*[@data-component-type='s-search-result'][2]")
+//    private iElement eleSecondProduct;
 
-    @FindBy(how = How.XPATH, using = "//*[@class='a-popover-inner']/ul/li/a[text()='Price: High to Low']")
-    private iElement elePriceHighToLow;
+    public static Element eleSortBy() {
+        return new Element(getElement("xpath","//span[*[text()='Sort by:']]"),"Sort by dropdown","product listing page");
+    }
 
-    @FindBy(how = How.XPATH, using = "//*[@data-component-type='s-search-result'][2]")
-    private iElement eleSecondProduct;
+    public static Element elePriceHighToLow() {
+        return new Element(getElement("xpath","//*[@class='a-popover-inner']/ul/li/a[text()='Price: High to Low']"),"Price high to low option","product listing page");
+    }
 
+    public static Element eleSecondProduct() {
+        return new Element(getElement("xpath","//*[@data-component-type='s-search-result'][2]"),"Second product from search listing","product listing page");
+    }
     public void sortByPriceHighToLow() throws InterruptedException {
-        eleSortBy.click();
+        eleSortBy().click();
         //Thread.sleep(1000);
-        elePriceHighToLow.click();
+        elePriceHighToLow().click();
         //Thread.sleep(1000);
     }
 
     public ProductDetailPage clickOnSecondProductListing() throws InterruptedException {
-        eleSecondProduct.click();
+        eleSecondProduct().click();
         //Thread.sleep(1000);
         return GetInstance(ProductDetailPage.class);
     }
